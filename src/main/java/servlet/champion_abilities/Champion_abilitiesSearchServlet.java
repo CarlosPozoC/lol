0355ae6161afa76f2b81918aadf445c8936b32a1
CarlosPozoC <cpozocatalan@gmail.com>
2022-11-29 08:27:49 +0100

    Comienzo ha hacer el segundo ejercicio, dejo el primero para el final, el tercero por suerte ya esta hecho de base en el proyecto, creo de momento dos .jsp uno para la busqueda y otro para el listado junto con un servlet llamado champion_abilitiesSearch

---
+++ src/main/java/servlet/champion_abilities/Champion_abilitiesSearchServlet.java
@@ -0,0 +1,41 @@
+package servlet.champion_abilities;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import models.Champion_abilities;
+import repositories.Champion_abilitiesRepository;
+import servlet.BaseServlet;
+
+@WebServlet(name="Champion_abilitiesSearchServlet", value="/Champion_abilitiesSearch")
+public class Champion_abilitiesSearchServlet extends BaseServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doYourThing(req, resp);
+	}
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		//doNothing()
+	}
+
+	private void doYourThing(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		Champion_abilitiesRepository repository = new Champion_abilitiesRepository();
+		Integer rango_1 =Integer.parseInt(req.getParameter("rango_1"));
+		Integer rango_2 =Integer.parseInt(req.getParameter("rango_2"));
+		List<Champion_abilities> champion_abilitiesList = repository.findByRango(rango_1,rango_2);
+		req.setAttribute("Champion_abilities", champion_abilitiesList);
+		redirect(req, resp, "/Champion_abilities/Champion_abilitiesSearchList.jsp");
+	}
+}
---
+++ src/main/webapp/Champion_abilities/Champion_abilitiesSearchList.jsp
@@ -0,0 +1,35 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8"%>
+
+<%@ taglib prefix="c"  uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Listado habilidades por rango</title>
+</head>
+<body>
+<h1>Listado habilidades por rango</h1>
+<form action="/Champion_abilitiesSearch" method="post">
+    <br/>
+    <a href="../index.jsp">Menu principal</a>
+    <br/>
+    <input type="submit"/>
+</form>
+
+<table>
+    <tr><td>Id</td><td>Champion</td><td>Champion_name</td><td>Champion_description</td><td>Effect</td><td>Cost</td><td>Rango</td></tr>
+    <c:forEach items="${Champion_abilities}" var="ca">
+        <tr>
+            <td>${ca.getId()}</td>
+            <td>${ca.getChampion()}</td>
+            <td>${ca.getChampion_name()}</td>
+            <td>${ca.getChampion_description()}</td>
+            <td>${ca.getEffect()}</td>
+            <td>${ca.getCost()}</td>
+            <td>${ca.getRango()}</td>
+        </tr>
+    </c:forEach>
+</table>
+</body>
+</html>
